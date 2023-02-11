da439e80de3a8007a84a51adc6215db8703e4cf6
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2023-02-11 16:19:11 -0500

    setup-secret.sh rough working version

--- scripts/setup-secret.sh
+++ scripts/setup-secret.sh
@@ -1,13 +1,30 @@
 # This script should run before the helm chart. This will create the namespace that we are going to be
 # running postgres within and creating a secret within that namespace.
-postgres_namespace = "postgres"
-postgres_secret_name = "postgres-secret"
+postgres_namespace="postgres"
+postgres_secret_name="postgres-secret"
 
-read -p "Enter POSTGRES_USER: " postgres_user
-read -s -p "Enter POSTGRES_PASSWORD: " posgres_password
+read -p "Enter POSTGRES_USER (admin): " postgres_user
+postgres_user=${postgres_user:-admin}
+read -s -p "Enter POSTGRES_PASSWORD: " postgres_password
 
 echo "Setting up namespace $postgres_namespace"
-kubectl create namespace $postgres_namespace
+# We could use kubectl create if we want, but it is not idempotent so this way is preferable
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $postgres_namespace
+EOF
 
 echo "Creating secret named $postgres_secret_name"
-
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+type: Opaque
+metadata:
+  namespace: $postgres_namespace
+  name: $postgres_secret_name
+data:
+  POSTGRES_USER: $(echo -n $postgres_user | base64 -w0)
+  POSTGRES_PASSWORD: $(echo -n $postgres_password | base64 -w0)
+EOF
